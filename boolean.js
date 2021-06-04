5e09e4977ff5929d086f6e6841a575473b3e9f07
unknown <vulgofariaspessoal@gmail.com>
2021-06-04 10:00:23 -0300

    add boolean.js

---
+++ boolean.js
@@ -0,0 +1,24 @@
+/*
+    Boolean
+
+    * somente 2 valores
+
+    true = verdadeiro
+    false = falso
+
+*/
+
+console.log(true) // verdadeiro
+
+console.log(false) // falso
+
+// Exemplos
+
+/*
+> vou fazer café
+    > pegar a xicara
+        > a xícara está suja?
+            > se sim (true), devo limpar
+                > depois de limpa, posso usar
+            > se não (false), posso usar
+*/
