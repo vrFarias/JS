f585271802c6d694b649f28e2993ecea2924c1da
unknown <vulgofariaspessoal@gmail.com>
2021-06-04 09:50:36 -0300

    add strings.js

---
+++ strings.js
@@ -0,0 +1,20 @@
+/*
+    String
+
+    * Cadeia de caracteres
+
+    "" aspas duplas
+    '' aspas simples
+    `` template literals ou template strings
+
+*/
+console.log("Vinicius")
+
+console.log('Vinicius')
+
+console.log(`
+esse permite
+multiplas linhas
+`)
+
+console.log(`permite colocar expressões dentro da propria string ${1+1}`)
