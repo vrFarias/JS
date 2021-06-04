939913763704c28defd807afd5a772a2b438c341
unknown <vulgofariaspessoal@gmail.com>
2021-06-04 10:07:21 -0300

    add null-undefined.js

---
+++ null-undefined.js
@@ -0,0 +1,16 @@
+/*
+    Undefined
+
+    * indefinido
+
+    Null
+
+    * nulo
+    * objeto que não possui nada dentro
+    * diferente de indefinido
+
+*/
+
+console.log(undefined) // indefinido = nunca foi definido
+
+console.log(null) // nulo = uma resposta ou um retorno que resultou em nada
