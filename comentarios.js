704dc4153b15b4b188451b3517b36357f2d95f5d
unknown <vulgofariaspessoal@gmail.com>
2021-06-04 09:34:47 -0300

    first commit

---
+++ comentarios.js
@@ -0,0 +1,10 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+    Aqui eu tenho um comentário multi linha
+    ou também comentário de bloco
+*/
+
+// O código abaixo escreve alguma mensagem no devtools
+
+console.log("Bem vindos ao starter") // alguma coisa
