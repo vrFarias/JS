7d9c578651a46f698bd5a51cf26ca447c147039b
unknown <vulgofariaspessoal@gmail.com>
2021-06-04 10:12:15 -0300

    add object.js

---
+++ object.js
@@ -0,0 +1,19 @@
+/*
+    Object
+
+    * objeto
+    * propriedades / atributos
+    * funcionalidades / métodos
+    * dado estrutural
+
+    { propriedade: "valor" }
+
+*/
+
+console.log({
+    nome: "Vinicius",
+    idade: 24,
+    andar: function(){
+        console.log('andar')
+    }
+})
