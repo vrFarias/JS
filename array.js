4cef71e84cf4c27e5fe5e0c96c0c77895be86bb2
unknown <vulgofariaspessoal@gmail.com>
2021-06-05 17:02:52 -0300

    add array.js

---
+++ array.js
@@ -0,0 +1,17 @@
+/*
+    Array (Vetores)
+
+    * uma lista
+    * agrupamento de dados
+
+    ["Vinicius", 24]
+    
+*/
+
+console.log(["Vinicius", 24])
+
+console.log([
+    "Leite",
+    "Ovo",
+    "Queijo"
+]) // Aceita quebra de linhas
