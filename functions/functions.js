7b9722d8f24eff574e55056f0b15566e7f2ecbaf
unknown <vulgofariaspessoal@gmail.com>
2021-06-06 11:07:10 -0300

    funcions+

--- functions/functions.js
+++ functions/functions.js
@@ -1,8 +1,10 @@
 /*
-    Prototype
+    Type conversion (typecasting) vs Type coersion
+        * Type conversion - Eu, explicitamente altero um tipo de dado através de uma função
+        * Type coersion - O JS, coersivamente altera o tipo de dado de forma automática
 
-    * prototype-based language
-    * prototype chain
-    * __proto__
+    * Alteração de um tipo de dado para outro tipo
 */
 
+console.log(Number('9') + 5)
+console.log('9' + 5)
