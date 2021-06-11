e8b19d9b541ecd4604f4e8123ac852fe9046028a
unknown <vulgofariaspessoal@gmail.com>
2021-06-11 13:42:41 -0300

    alterações

--- functions/manipulandoArrays_adicionarEremover.js
+++ functions/manipulandoArrays_adicionarEremover.js
@@ -9,16 +9,16 @@ techs.push("Node.JS")
 techs.unshift("SQL")
 
 // remover do fim
-// techs.pop()
+techs.pop()
 
 // remover do começo
-// techs.shift()
+techs.shift()
 
 // pegar somente alguns elementos do Array
-// console.log(techs.slice(1, 2))
+console.log(techs.slice(1, 2))
 
 // remover 1 ou mais itens em qualquer posição do Array
-// techs.splice(1, 1)
+techs.splice(1, 1)
 
 // encontrar a posição de um elemento no Array
 let index = techs.indexOf("JavaScript")
